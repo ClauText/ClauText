4ad0788cf216c9a5c2828da13fe97d577cdba835
DongWook Lee <vztpv@naver.com>
2018-08-16 11:08:38 +0900

    Add files via upload

--- include/wiz/array_map.h
+++ include/wiz/array_map.h
@@ -15,20 +15,70 @@ namespace wiz {
 		using const_iterator = typename std::vector<std::pair<Key, Data>>::const_iterator;
 	private:
 		std::vector<std::pair<Key, Data>> arr;
-		size_t no_sort_start_idx = 1;
+	public:
+		explicit ArrayMap(size_t reserve_num = 0) {
+			if (reserve_num > 0) {
+				arr.reserve(reserve_num);
+			}
+		}
+	private:
+		void _sort(const size_t last, const size_t num) {
+			size_t count = num >> 1; // two group, count is size of one group.
+			
+			std::sort(begin() + last - 2 * count + 1, begin() + last + 1);//_merge(last - 2 * count + 1, last - count + 1, count);
+		}
+		iterator binary_search(long long left, long long right, const Key& key)
+		{
+			long long middle = (left + right) >> 1; // / 2;
+
+			while (left <= right) {
+				auto& x = arr[middle].first;
+
+				if (x == key) {
+					return begin() + middle;
+				}
+				else if (x < key) {
+					left = middle + 1;
+				}
+				else {
+					right = middle - 1;
+				}
+				middle = (left + right) >> 1;
+			}
+
+			return end();
+		}
+		const_iterator binary_search(long long left, long long right, const Key& key) const
+		{
+			long long middle = (left + right) >> 1; // / 2;
+
+			while (left <= right) {
+				auto& x = arr[middle].first;
+
+				if (x == key) {
+					return begin() + middle;
+				}
+				else if (x < key) {
+					left = middle + 1;
+				}
+				else {
+					right = middle - 1;
+				}
+				middle = (left + right) >> 1;
+			}
+
+			return end();
+		}
+
 	public:
 		bool empty() const {
-			
 			return arr.empty();
 		}
 		auto size() const {
-			
 			return arr.size();
 		}
 		void clear() {
-			
 			arr.clear();
-			no_sort_start_idx = 1;
 		}
 	public:
 		const_iterator begin() const {
@@ -49,36 +99,46 @@ namespace wiz {
 				return end();
 			}
 			if (arr.size() == 1) {
-				return key == arr[0].first ? begin() : end();
+				return key == arr[0].first ? begin() + 0 : end();
 			}
 
-			// binary search
+			// forest binary search
 			{
-				long long left = 0, right = no_sort_start_idx - 1;
-				long long middle = (left + right) / 2;
+				// size_t : unsigned ~?
+				size_t num = 2;
+				size_t len = arr.size();
 
-				while (left <= right) {
-					if (arr[middle].first == key) {
-						return begin() + middle;
+				if (1 == len % 2) {
+					if (key == arr.back().first) {
+						return begin() + arr.size() - 1;
 					}
-					else if (arr[middle].first < key) {
-						left = middle + 1;
+					--len;
+				}
+				const size_t _len = len;
+				long long left = std::max((long long)0, (long long)_len - 2);
+				size_t right = _len - 1;
+
+				len = len >> 1;
+
+				while (len > 0) {
+					if (1 == len % 2) {
+						auto x = binary_search(left, right, key);
+						if (x != end()) {
+							return x;
+						}
+						right = left - 1;
 					}
 					else {
-						right = middle - 1;
+
 					}
-					middle = (left + right) / 2;
+
+					len = len >> 1;
+					num = num * 2;
+
+					left = right - num + 1;
 				}
 			}
 
-			// linear search
-			{
-				for (long long i = no_sort_start_idx; i < arr.size(); ++i) {
-					if (arr[i].first == key) {
-						return begin() + i;
-					}
-				}
-			}
 			return end();
 		}
 		const_iterator find(const Key& key) const {
@@ -89,46 +149,88 @@ namespace wiz {
 				return key == arr[0].first ? begin() : end();
 			}
 
-			// binary search
+			// forest binary search
 			{
-				long long left = 0, right = no_sort_start_idx - 1;
-				long long middle = (left + right) / 2;
-				
-				while (left <= right) {
-					if (arr[middle].first == key) {
-						return begin() + middle;
+				// size_t : unsigned ~?
+				size_t num = 2;
+				size_t len = arr.size();
+
+				if (1 == len % 2) {
+					if (key == arr.back().first) {
+						return begin() + arr.size() - 1;
 					}
-					else if (arr[middle].first < key) {
-						left = middle + 1;
+					--len;
+				}
+				const size_t _len = len;
+				long long left = std::max((long long)0, (long long)_len - 2);
+				size_t right = _len - 1;
+
+				len = len >> 1;
+
+				while (len > 0) {
+					if (1 == len % 2) {
+						auto x = binary_search(left, right, key);
+						if (x != end()) {
+							return x;
+						}
+						right = left - 1;
 					}
 					else {
-						right = middle - 1;
+
 					}
 
-					middle = (left + right) / 2;
+					len = len >> 1;
+					num = num * 2;
+
+					left = right - num + 1;
 				}
 			}
 
-			// linear search
-			{
-				for (long long i = no_sort_start_idx; i < arr.size(); ++i) {
-					if (arr[i].first == key) {
-						return begin() + i;
-					}
-				}
-			}
 			return end();
 		}
 	public:
 		// using maybe quick sort, cf) insertion sort?
-		void insert(const std::pair<Key, Data>& value, bool do_sort = false) {
+		void insert(const std::pair<Key, Data>& value) {
 			if (end() != find(value.first)) { return; }
 			arr.push_back(value);
 
-			if (do_sort && no_sort_start_idx < arr.size()) {
-				std::sort(arr.begin(), arr.end());
-				no_sort_start_idx = arr.size();
+			// -sort
+			{
+				// size_t : unsigned ~?
+				size_t num = 2;
+				size_t len = arr.size();
+
+				if (1 == len % 2) {
+					return;
+				}
+
+				len = len - 1;
+				size_t x = arr.size() - 1;
+				size_t temp = arr.size() ^ (arr.size() - 1);
+
+				// when len is even... > 0
+				while (len > 0 && temp > 0) {
+					if (1 == len % 2) {
+						//if (x - num < 0) {
+						//	break;
+						//}
+						_sort(x, num);
+					}
+					len = len >> 1;
+					num = num * 2;
+					temp = temp >> 1;
+				}
 			}
+
+			// debug
+
+			//std::cout << std::endl;
+			//std::cout << std::endl;
+			//for (auto& x : arr) {
+			//	std::cout << x.first << " ";
+			//}
+			//std::cout << std::endl;
+			//std::cout << std::endl;
 		}
 		Data& at(const Key& key) {
 			return (*this)[key];
