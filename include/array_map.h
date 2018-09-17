0ef552b4e50d415894f2c1149b537aaec8410260
DongWook Lee <vztpv@naver.com>
2018-09-17 15:31:36 +0900

    bug fix

---
+++ include/array_map.h
@@ -0,0 +1,260 @@
+﻿
+#ifndef BINARY_SEARCH_TREE_H
+#define BINARY_SEARCH_TREE_H
+
+#include <vector>
+#include <algorithm>
+#include <utility>
+
+// like set, map,   instead of map!
+namespace wiz {
+	template <class Key, class Data>
+	class ArrayMap {
+	public:
+		using iterator = typename std::vector<std::pair<Key, Data>>::iterator;
+		using const_iterator = typename std::vector<std::pair<Key, Data>>::const_iterator;
+	private:
+		std::vector<std::pair<Key, Data>> arr;
+	public:
+		explicit ArrayMap(size_t reserve_num = 0) {
+			if (reserve_num > 0) {
+				arr.reserve(reserve_num);
+			}
+		}
+	private:
+		void _sort(const size_t last, const size_t num) {
+			size_t count = num >> 1; // two group, count is size of one group.
+
+			std::inplace_merge(arr.begin() + (last - 2 * count + 1), arr.begin() + (last - count + 1), arr.begin() + (last + 1));
+
+			//std::sort(begin() + last - 2 * count + 1, begin() + last + 1);//_merge(last - 2 * count + 1, last - count + 1, count);
+		}
+		iterator binary_search(long long left, long long right, const Key& key)
+		{
+			long long middle = (left + right) >> 1; // / 2;
+
+			while (left <= right) {
+				auto& x = arr[middle].first;
+
+				if (x == key) {
+					return begin() + middle;
+				}
+				else if (x < key) {
+					left = middle + 1;
+				}
+				else {
+					right = middle - 1;
+				}
+				middle = (left + right) >> 1;
+			}
+
+			return end();
+		}
+		const_iterator binary_search(long long left, long long right, const Key& key) const
+		{
+			long long middle = (left + right) >> 1; // / 2;
+
+			while (left <= right) {
+				auto& x = arr[middle].first;
+
+				if (x == key) {
+					return begin() + middle;
+				}
+				else if (x < key) {
+					left = middle + 1;
+				}
+				else {
+					right = middle - 1;
+				}
+				middle = (left + right) >> 1;
+			}
+
+			return end();
+		}
+
+	public:
+		bool empty() const {
+			return arr.empty();
+		}
+		auto size() const {
+			return arr.size();
+		}
+		void clear() {
+			arr.clear();
+		}
+	public:
+		const_iterator begin() const {
+			return arr.begin();
+		}
+		const_iterator end() const {
+			return arr.end();
+		}
+		iterator begin() {
+			return arr.begin();
+		}
+		iterator end() {
+			return arr.end();
+		}
+
+		iterator find(const Key& key) {
+			if (arr.empty()) {
+				return end();
+			}
+			if (arr.size() == 1) {
+				return key == arr[0].first ? begin() + 0 : end();
+			}
+
+			// forest binary search
+			{
+				// size_t : unsigned ~?
+				size_t num = 2;
+				size_t len = arr.size();
+
+				if (1 == len % 2) {
+					if (key == arr.back().first) {
+						return begin() + arr.size() - 1;
+					}
+					--len;
+				}
+				const size_t _len = len;
+				long long left = std::max((long long)0, (long long)_len - 2);
+				size_t right = _len - 1;
+
+				len = len >> 1;
+
+				while (len > 0) {
+					if (1 == len % 2) {
+						auto x = binary_search(left, right, key);
+						if (x != end()) {
+							return x;
+						}
+						right = left - 1;
+					}
+					else {
+
+					}
+
+					len = len >> 1;
+					num = num * 2;
+
+					left = right - num + 1;
+				}
+			}
+
+			return end();
+		}
+		const_iterator find(const Key& key) const {
+			if (arr.empty()) {
+				return end();
+			}
+			if (arr.size() == 1) {
+				return key == arr[0].first ? begin() : end();
+			}
+
+			// forest binary search
+			{
+				// size_t : unsigned ~?
+				size_t num = 2;
+				size_t len = arr.size();
+
+				if (1 == len % 2) {
+					if (key == arr.back().first) {
+						return begin() + arr.size() - 1;
+					}
+					--len;
+				}
+				const size_t _len = len;
+				long long left = std::max((long long)0, (long long)_len - 2);
+				size_t right = _len - 1;
+
+				len = len >> 1;
+
+				while (len > 0) {
+					if (1 == len % 2) {
+						auto x = binary_search(left, right, key);
+						if (x != end()) {
+							return x;
+						}
+						right = left - 1;
+					}
+					else {
+
+					}
+
+					len = len >> 1;
+					num = num * 2;
+
+					left = right - num + 1;
+				}
+			}
+
+			return end();
+		}
+	public:
+		// using maybe quick sort, cf) insertion sort?
+		// different point compared by std::map?
+		void insert(std::pair<Key, Data>& value) {
+			if (end() != find(value.first)) { find(value.first)->second = value.second; return; }
+			arr.push_back(value);
+
+			// -sort
+			{
+				// size_t : unsigned ~?
+				size_t num = 2;
+				size_t len = arr.size();
+
+				if (1 == len % 2) {
+					return;
+				}
+
+				len = len - 1;
+				size_t x = arr.size() - 1;
+				size_t temp = arr.size() ^ (arr.size() - 1);
+
+				// when len is even... > 0
+				while (len > 0 && temp > 0) {
+					if (1 == len % 2) {
+						//if (x - num < 0) {
+						//	break;
+						//}
+						_sort(x, num);
+					}
+					len = len >> 1;
+					num = num * 2;
+					temp = temp >> 1;
+				}
+			}
+
+			// debug
+
+			//std::cout << std::endl;
+			//std::cout << std::endl;
+			//for (auto& x : arr) {
+			//	std::cout << x.first << " ";
+			//}
+			//std::cout << std::endl;
+			//std::cout << std::endl;
+		}
+		Data& at(const Key& key) {
+			return (*this)[key];
+		}
+		const Data& at(const Key& key) const {
+			return find(key)->second;
+		}
+		Data& operator[](const Key& key) {
+			iterator idx;
+			if (end() == (idx = find(key))) {
+				std::pair temp(key, Data());
+				insert(temp);
+				return find(key)->second;
+			}
+			else {
+				return idx->second;
+			}
+		}
+	};
+}
+
+
+#endif
+
---
+++ include/stacks.h
@@ -0,0 +1,405 @@
+﻿#ifndef STACKS_H_INCLUDED
+#define STACKS_H_INCLUDED
+
+#include "global.h"
+#include "wizardError.h"
+#include "newarrays.h"
+
+/// #define STACKS_DEBUG
+
+namespace wiz {
+
+class StackEmptyError : public wiz::Error
+{
+public:
+    StackEmptyError() : wiz::Error( "sEmptyError" )
+    {
+
+    }
+};
+class StackFullError : public wiz::Error
+{
+public:
+    StackFullError() : wiz::Error( "sFullError" )
+    {
+
+    }
+};
+
+template <class T>
+class Stack
+{
+    class Element
+    {
+    public:
+        Element* next;
+        T data;
+    public:
+        //
+        explicit Element( const T& d=T() )
+        {  // 湲곕낯 ??? // ?뺤? 蹂듭궗.
+            data = d; // why this?
+            next = nullptr;
+        }
+    };
+private:
+    void copy( const Stack<T>& s )
+    {
+        /// this->clear();
+        Element* pE = s.Rear;
+        Stack<T> temp;
+        while( nullptr != pE )
+        {
+            temp.push( pE->data );
+            pE = pE->next;
+        }
+        pE = temp.Rear;
+        while( nullptr != pE )
+        {
+            this->push( pE->data );
+            pE = pE->next;
+        }
+    }
+public:
+    Stack( const Stack<T>& s )
+    {
+        num = 0;
+        Rear = nullptr;
+        copy( s );
+    }
+    Stack<T>& operator=( const Stack<T>& s )
+    {
+        if( this->Rear == s.Rear ) { return *this; }
+        clear();
+        copy( s );
+		num = s.num;
+        return *this;
+    }
+    Stack( Stack<T>&& s )
+    {
+        Rear = s.Rear;
+        num = s.num;
+
+        s.Rear = nullptr;
+        s.num = 0;
+    }
+    Stack<T>& operator=( Stack<T>&& s )
+    {
+        if( Rear == s.Rear ) { return *this; }
+        clear();
+        Rear = s.Rear;
+        num = s.num;
+
+        s.Rear = nullptr;
+        s.num = 0;
+        return *this;
+    }
+private:
+    Element* Rear;// 瑗щ━???곗씠?곕? ?ｋ뒗??
+    int num;  // for toArray
+public:
+    explicit Stack() : Rear( nullptr ), num( 0 ) { }
+    ~Stack(){
+        clear();
+    }
+    void clear() {
+        while( !isEmpty() ){
+            pop();
+        }
+    }
+    bool isEmpty()const{ // 鍮꾩뿀?? //
+        return nullptr == Rear; // num <= 0
+    }
+    bool empty() const{ return isEmpty(); }
+    //
+    void push( const T& p ){ // ?ｊ린
+        Element* temp = new Element(p); //
+        //Rear
+        temp->next = Rear; // Rear媛 媛瑜댄궎??媛믪쓣 媛瑜댄궎寃??쒕떎.
+        Rear = temp; //?덈줈 ?ㅼ뼱?ㅻ뒗 寃껋쓣 湲곗??쇰줈 ?쒕떎
+        //
+        num++;
+    }
+    //
+    T pop(){ // 鍮쇨린... cf  Rear媛 怨좎젙, ?곗씠?곌? ?대룞?섎뒗 寃?泥섎읆 ?됰룞.
+        // ?덉쇅 ?뚯뒪??.
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        //
+        Element* temp = Rear; // ?꾩옱 媛瑜댄궎??寃?
+        Rear = Rear->next; // Rear?먮뒗 ..
+        T returnTemp = (temp->data);
+        delete temp;
+        //
+        num--;
+        //
+        return returnTemp; //return???놁뼱??而댄뙆?쇱씠 ?섎떎??;; - in CodeBlocks.
+    }
+    T getTop()const
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        return (Rear->data);
+    }
+    void setTop( const T& val )
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        Rear->data = val;
+    }
+    T& top()
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            return Rear->data;
+        }
+    }
+    const T& top() const
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            return Rear->data;
+        }
+    }
+
+    // ?ㅽ깮???ｋ뒗??
+    Stack<T>& operator<<( const T& p ){
+        push(p);
+        return *this;
+    }
+
+    Array<T> toArray()const
+    {
+        if( isEmpty() ) { return Array<T>(); }
+        Array<T> arrayTemp( num );
+        Element* eTemp = Rear; // ElementTemp
+        for( int i=0; i < arrayTemp.size(); i++ )
+        {
+            arrayTemp[i] = eTemp->data;
+            eTemp = eTemp->next;
+        }
+
+        return arrayTemp;
+    }
+
+    int getNum()const { return num; }
+};
+
+template <class T>
+class ArrayStack
+{
+public:
+	void reserve(long long size)
+	{
+		p.reserve(size);
+	}
+public:
+    ArrayStack( const ArrayStack<T>& s )
+		:p(s.p), count(s.count)
+	{
+		//
+    }
+    ArrayStack<T>& operator=( const ArrayStack<T>& s )
+    {
+        if( p == s.p ) { return *this; }
+        p.DoEmpty();
+        p = s.p;
+        count = s.count;
+        return *this;
+    }
+    ArrayStack<T>& operator=( ArrayStack<T>&& s )
+    {
+        if( p == s.p ) { return *this; }
+        p = std::move( s.p );
+        count = s.count;
+
+        s.p = Array<T>( 1 );
+        s.count = 0;
+        return *this;
+    }
+    ArrayStack( ArrayStack<T>&& s )
+    {
+        p = std::move( s.p );
+        count = s.count;
+
+        s.p = Array<T>( 1 );
+        s.count = 0;
+    }
+private:
+    Array<T> p;
+    int count;
+public:
+	explicit ArrayStack()
+		:count(0)
+	{
+		p = Array<T>();
+	}
+
+    explicit ArrayStack( const int size )
+    : count(0)
+    {
+        #ifdef STACKS_DEBUG
+        wiz::checkUnderError( 0, size, 1 ); // must size >= 1
+        #endif
+        p = Array<T>(size);
+    }
+    virtual ~ArrayStack() {  } // added!! 2012.06.04
+public:
+    // push
+    void push( const T& data )
+    {
+		if (p.empty()) {
+			p = wiz::Array<T>(1);
+		}
+
+        if( count >= p.size() )
+        {
+            // changed 2012.06.04
+            p.expand();
+        }
+
+        p[count] = data;
+        count++;
+    }
+	void push(const T&& data)
+	{
+		if (p.empty()) {
+			p = wiz::Array<T>(1);
+		}
+
+		if (count >= p.size())
+		{
+			// changed 2012.06.04
+			p.expand();
+		}
+
+		p[count] = std::move(data);
+		count++;
+	}
+    // pop
+    T pop()
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            T temp = p[count-1];
+          //  p[count-1] = T();
+            count--;
+            return temp;
+        }
+    }
+    void clear()
+    {
+        for( int i=0; i < count; i++ )
+        {
+            p[i] = T();
+        }
+        count = 0;
+    }
+	const T& Top()const { return p[count - 1]; }
+	T& Top() { return p[count - 1]; }
+    T getTop()const
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            return p[count-1];
+        }
+    }
+    void setTop( const T& val )
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            p[count-1] = val;
+        }
+    }
+    T& top()
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            return p[count-1];
+        }
+    }
+    const T& top() const
+    {
+        if( isEmpty() )
+        {
+            throw StackEmptyError();
+        }
+        else
+        {
+            return p[count-1];
+        }
+    }
+
+    ArrayStack<T>& operator<<( const T& var )
+    {
+        push( var );
+
+        return *this;
+    }
+
+    bool isEmpty() const { return count <= 0; }
+    bool empty() const { return count <= 0; }
+    Array<T> toArray()const {
+        if( count <= 0 ) { return Array<T>(); }
+        Array<T> temp( count );
+
+        for( int i=0; i < temp.size(); i++ )
+        {
+            temp[i] = p[i];
+        }
+
+        return temp;
+    }
+    // int getMaxNum()const
+    int size()const { return count; }
+    int getNum()const
+    {
+        return count;
+    }
+    int getNumMax()const
+    {
+        return p.size();
+    }
+    T& operator[] ( const int idx )
+    {
+        /// #define?  assert( idx is index 0 ~ count-1 )
+        return p[idx];
+    }
+	const T& operator[](const int idx) const
+	{
+		return p[idx];
+	}
+};
+
+}
+#endif // STACKS_H_INCLUDED
