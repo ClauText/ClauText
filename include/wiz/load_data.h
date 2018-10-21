b8c13bf188f8a76577eb7fcedb93dcd48cf988db
DongWook Lee <vztpv@naver.com>
2018-10-21 17:58:21 +0900

    Add files via upload

--- include/wiz/load_data.h
+++ include/wiz/load_data.h
@@ -6743,7 +6743,7 @@ namespace wiz {
 				for (int i = 0; i < parameters.size(); ++i) {
 					if (wiz::String::startsWith(operand, "$parameter.")
 						&& (x = parameters.find(wiz::String::substring(operand, 11))) != parameters.end()) {
-						return x->key.second;
+						return x->second;
 					}
 				}
 				return "";
